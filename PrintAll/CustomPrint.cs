f9ba735cadfd2a7d82b4e4d3dcb9c22ff3423fe8
Nirjhar Choudhury <nirjhar_choudhury@trimble.com>
2020-06-28 21:00:30 -0400

    First checkin

---
+++ PrintAll/CustomPrint.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PrintAll
+{
+    public class CustomPrint
+    {
+        private string name;
+
+        public void Print()
+        {
+            Console.WriteLine("Printing from Print");
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine($"Name set as {this.name}");
+        }
+
+        public void Print(string name)
+        {
+            Console.WriteLine($"Name passed: {name}");
+        }
+
+        private void PrintPrivate()
+        {
+            Console.WriteLine("Printing from private");
+        }
+
+        public static void PrintStatic()
+        {
+            Console.WriteLine("Static method");
+        }
+
+        public string Name => name;
+
+        public static string StaticName => "Static property name";
+    }
+}
---
+++ Reflection.Demo/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Reflection.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.LoadFrom(@"C:\code\reflection-demo\PrintAll\bin\Debug\netcoreapp3.1\PrintAll.dll");
+
+            foreach (var type in assembly.GetTypes())
+            {
+                Console.WriteLine($"Type: {type.Name}");
+                Console.WriteLine("============================");
+                var instance = Activator.CreateInstance(type);
+
+                foreach (var field in type.GetFields(BindingFlags.NonPublic | 
+                    BindingFlags.Instance |
+                    BindingFlags.DeclaredOnly))
+                {
+                    Console.WriteLine($"Field: {field.Name}");
+                    field.SetValue(instance, "Frodo");
+                }
+                Console.WriteLine("============================");
+
+                foreach (var method in type.GetMethods(BindingFlags.Public |
+                    BindingFlags.NonPublic |
+                    BindingFlags.Instance |
+                    BindingFlags.DeclaredOnly)
+                    .Where(m => !m.IsSpecialName))
+                {
+                    Console.WriteLine($"Method: {method.Name}");
+                    if (method.GetParameters().Length > 0)
+                    {
+                        method.Invoke(instance, new[] { "Bilbo" });
+                    }
+                    else if (method.ReturnType.Name != "Void")
+                    {
+                        var returnedValue = method.Invoke(instance, null);
+                        Console.WriteLine($"Returned value from method: {returnedValue}");
+                    }
+                    else
+                    {
+                        method.Invoke(instance, null);
+                    }
+                }
+                Console.WriteLine("============================");
+
+                foreach (var property in type.GetProperties())
+                {
+                    Console.WriteLine($"Property: {property.Name}");
+                    var propertyValue = property.GetValue(instance);
+                    Console.WriteLine($"Property value: {propertyValue}");
+                }
+
+            }
+
+        }
+    }
+}
